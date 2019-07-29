a1b55c26d92dba2a44ac80f5aa10c1fb6f674954
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-29 17:12:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,26 +7,18 @@ function theBeatlesPlay(musicians, instruments) {
     return allMusicians;
 }
 
-function johnLennonFacts(facts) {
-  
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
 
 
-describe('johnLennonFacts', function(){
-    it("returns an array of strings with exclamation points", function() {
-      expect(johnLennonFacts([
-        "He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"
-      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
 
-      expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
-    });
-  });
 
-  
 
