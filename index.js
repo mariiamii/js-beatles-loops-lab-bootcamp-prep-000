4f68748365a7bc2ff08f789621ea9965de47c9b7
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-29 17:05:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ function theBeatlesPlay(musicians, instruments) {
   return allMusicians;
 }
 
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
 
 
 
