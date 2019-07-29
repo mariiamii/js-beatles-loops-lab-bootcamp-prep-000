58bd861e7df8014dabfc32df0099c67ff7951a2e
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-29 17:15:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ function johnLennonFacts() {
     return newFacts;
 }
 
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
