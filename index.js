641be8a2c2a5981250dff81bcb0ea91e9f86a330
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-29 17:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var allMusicians ();
   for (let i = 0; i < musicians.length; i++) {
-  allMusicians.push
+  allMusicians.push(musicians[i] + " plays " + instruments[i]);
   return allMusicians;
 }
 
@@ -10,10 +10,3 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 
-describe('Beatles Loops', function() {
-  it("returns an array of strings containing what instruments each musician plays", function() {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
