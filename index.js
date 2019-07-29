4f8f724be77baf65a94245423a623240aaf24108
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-29 17:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var allMusicians ();
-  for (let i = 0; i < musicians.length; i++) {
-  allMusicians.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return allMusicians;
-}
-
 function theBeatlesPlay(players, instruments) {
     var allPlayers = [];
     for (var i=0; i < players.length; i++) {
